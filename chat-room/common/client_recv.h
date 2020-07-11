e7fc819af62bc1314882b7a1590d2fafc55d73b9
JJ8864 <1023921728@qq.com>
2020-07-11 11:09:58 +0800

    NEW

---
+++ chat-room/common/client_recv.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: client_recv.h
+	> Author:JJ 
+	> Mail:1023921728@qq.com 
+	> Created Time: Sat 11 Jul 2020 09:50:52 AM CST
+ ************************************************************************/
+
+#ifndef _CLIENT_RECV_H
+#define _CLIENT_RECV_H
+void *do_recv(void *arg);
+#endif
